469ea70ffbc39cad56b9c7625bc043d550c19bed
kubraaydemir <110557088+kubraaydemir@users.noreply.github.com>
2024-08-13 14:30:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stajLoginApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* KULLANICI GİRİŞ SAYFASI ÖRNEĞİ
+             KULLANICI ADI VE ŞİRFRE DOĞRU GİRİLİRSE HOŞGELDİNİZ SAYFASI YANLIŞ GİRİLİRSE HATA MESAJI TOPLAM 3 HAK VERELİM SONRA KİTLENSİN */
+            int kalanGirisYapmaHakki = 3;
+            while (true)
+            {
+                Console.WriteLine("Kullanıcı adınızı giriniz:");
+                string kullaniciAdi = Console.ReadLine();
+                Console.WriteLine("Şifrenizi giriniz:");
+                string sifre= Console.ReadLine();
+
+                if (kullaniciAdi == "stajyerGiris" && sifre == "2024stajyer")
+                {
+                    Console.WriteLine("Hoşgeldiniz.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı veya şifre yanlış girildi");
+                    if (kalanGirisYapmaHakki > 0)
+                    {
+                        kalanGirisYapmaHakki--;
+                    }
+                    if (kalanGirisYapmaHakki == 0)
+                    {
+                        Console.WriteLine("Giriş yapma hakkınız dolmuştur. Lütfen yetkiliyle görüşünüz.");
+                        break;
+                    }
+                }
+                
+                
+           } 
+            Console.ReadLine();
+               
+        }
+    }
+}
